dbb65065bce5025782d08dbace98c8a4be1ec056
shivaraj110 <shivarajchandaragi9@gmail.com>
2026-04-24 22:05:12 +0530

    v0.1.4

--- src/components/footer.ts
+++ src/components/footer.ts
@@ -27,7 +27,7 @@ export function createFooter(renderer: RenderContext, theme: Theme) {
 
   const versionText = new TextRenderable(renderer, {
     id: "footer-version",
-    content: " v0.1.3",
+    content: " v0.1.4",
     fg: theme.colors.white,
     attributes: TextAttributes.DIM,
   });
